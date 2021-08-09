25e8fce9b6c2c895ea791749a9079e6aaf92a50d
Blake Schumann <blakeschumann15@gmail.com>
2021-08-09 20:16:14 +0930

    editing init app and readme file functions

--- index.js
+++ index.js
@@ -125,3 +125,23 @@ const questions = [
     },
 ];
 
+// Function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err)
+            throw err;
+        console.log('Success! Information transferred to the README!')
+    });
+};
+
+// Function to initialize app
+function init() {
+    inquirer.prompt(questions)
+    .then(function (userInput) {
+        console.log(userInput)
+        writeToFile("README.md", generateMarkdown(userInput));
+    });
+};
+
+// Function call to initialize app
+init();
