a6dfdc527026c7011c837a11e0af2bad88e6c558
Blake Schumann <blakeschumann15@gmail.com>
2021-08-19 19:25:16 +0930

    adding comments

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateMarkdown = require('./src/generateMarkdown.js')
+const generateMarkdown = require('./src/generateMarkdown')
 
 //Questions that will be asked to the user
 const questions = [
